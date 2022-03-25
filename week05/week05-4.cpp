750e2316a5a2c875d47c9e8692db431f1d1e6dc3
ww218 <g8wlc681208@gmail.com>
2022-03-25 11:48:07 +0800

    add week05

---
+++ week05/week05-4.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+char line[1000];
+int main()
+{
+    int T;
+    scanf("%d\n\n",&T);
+
+    for(int t=1 ; t<=T ; t++)///第t筆資料
+    {
+    	if(t>1)printf("\n");
+    	printf("現在讀到第%d大坨的資料\n",t);
+    	while(gets(line))
+    	{
+    		if(line[0]==0)break;///讀到跳行,break
+    	}
+    }
+}
