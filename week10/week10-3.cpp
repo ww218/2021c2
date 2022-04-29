cd50366ded8507bba163c3358232aee5b13a73f3
ww218 <g8wlc681208@gmail.com>
2022-04-29 12:23:43 +0800

    week09

---
+++ week10/week10-3.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>///使用物件class
+class Record///c++的物件，用class宣告，裡面有名字 分數
+{
+    public:///公開的欄位，可以被別人main使用
+    char name[30];
+    int grade;
+};
+Record student[100];///有100筆學生的資料
+int main()
+{
+    FILE * fin = fopen ("input.txt","r");
+    int N;
+    fscanf( fin, "%d", &N);///scanf("%d",&N)
+    for(int i=0;i<N;i++)
+    {
+        fscanf( fin, "%s %d", student[i].name, &student[i].grade);
+    }
+    for(int k=0;k<N-1;k++)
+    {
+        for(int i=0;i<N-1;i++)
+        {
+            if(student[i].grade < student[i+1].grade)
+            {
+                Record temp = student[i];
+                student[i] = student[i+1];
+                student[i+1]=temp;
+            }
+        }
+    }
+    for(int i=0;i<N;i++)
+    {
+        printf( "%s %d\n", student[i].name, student[i].grade);
+    }
+}
+
